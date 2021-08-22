e35309b5377b2c4b1c127adebe0973fc60c9fac2
Luis-Felipe-N <luisj2felipe09@gmail.com>
2021-08-22 19:52:24 -0300

    darkmode

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,12 +3,34 @@ import Link from 'next/link'
 import styles from './styles.module.scss'
 
 import { BiSun, BiMoon, BiFolder, BiFolderOpen, BiRightArrowAlt } from 'react-icons/bi'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import NavLink from '../NavLInk'
 
 export function Header() {
     const [ ligthMode, setLigthMode ] = useState(false)
-    const [ openMenu, setOpenMenu ] = useState(false)
+    const [ openMenu, setOpenMenu ] = useState()
+
+    useEffect(() => {
+        const mode = localStorage.getItem('ligthMode')
+        if (mode) {
+            document.body.classList.add('ligth-mode')
+            setLigthMode('ligth')
+        } else {
+            setLigthMode('dark')
+        }
+    }, [])
+
+
+    useEffect(() => {
+        console.log(ligthMode)
+        if (ligthMode == 'ligth') {
+            document.body.classList.add('ligth-mode')
+            localStorage.setItem('ligthMode', 'true')
+        } else if (ligthMode == 'dark') {
+            document.body.classList.remove('ligth-mode')
+            localStorage.removeItem('ligthMode')
+        }
+    }, [ligthMode])
 
     return (
         <header className={styles.headerContainer}>
@@ -24,17 +46,23 @@ export function Header() {
                         Projetos <BiRightArrowAlt />
                     </NavLink>
                 </nav>
-                <button
-                    className={styles.ligth_mode} 
-                    onClick={()=>{
-                        document.body.classList.toggle('ligth-mode')
-                        setLigthMode(!ligthMode)
-                    }}
-                >
-                    {
-                        ligthMode ? <BiMoon size='1.5rem' /> : <BiSun size='1.5rem' />
-                    }
-                </button>
+                {
+                    ligthMode == 'ligth'  ? (
+                        <button
+                            className={styles.ligth_mode}
+                            onClick={() => setLigthMode('dark')}
+                        >
+                            <BiMoon size='1.5rem' />
+                        </button>
+                    ) : (
+                        <button
+                            className={styles.ligth_mode}
+                            onClick={() => setLigthMode('ligth')}
+                        >
+                            <BiSun size='1.5rem' />
+                        </button>
+                    )
+                }
                 <button 
                     onClick={() => setOpenMenu(!openMenu)}
                     className={styles.btn_mobile}>
