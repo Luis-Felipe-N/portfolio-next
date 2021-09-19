eb01c495e08136da77a964ea454e112132cfea96
Luis-Felipe-N <luisj2felipe09@gmail.com>
2021-09-18 22:39:36 -0300

    add theme-color

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -7,6 +7,8 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <meta name="theme-color" content="var(--background)"/>
+                    {/* <meta http-equiv="X-UA-Compatible" content="ie=edge" /> */}
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
                     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600;700&family=Roboto+Mono:wght@400;500&display=swap" rel="stylesheet"/>
