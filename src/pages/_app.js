3b86e49cc46c24a55a9a2e7c9d2a6b70d339afa4
Luis-Felipe-N <luisj2felipe09@gmail.com>
2021-10-17 23:53:44 -0300

    change width ft perfil

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -49,15 +49,15 @@ function MyApp({ Component, pageProps }) {
             images: [
             {
                 url: '/perfil.jpeg',
-                width: 400,
-                height: 400,
+                width: 200,
+                height: 200,
                 alt: 'Imagem de perfil',
                 type: 'image/jpeg',
             },
             {
                 url: '/perfil.jpeg',
-                width: 400,
-                height: 400,
+                width: 200,
+                height: 200,
                 alt: 'Imagem de perfil',
                 type: 'image/jpeg',
             },
